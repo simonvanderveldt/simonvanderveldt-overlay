399757d1880a53a9e6259c290aecf2d201fc025e
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-03-01 16:12:20 +0100

    [travis] Enable caching

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -x
 
-# First update portage and install dependencies
-emerge-webrsync
-emerge dev-vcs/git repoman
+# Update the portage tree and install dependencies
+PORTAGE_RSYNC_EXTRA_OPTS="-q" emerge --sync
+emerge -q --buildpkg --usepkg dev-vcs/git repoman
 
 # Run the tests
 repoman full

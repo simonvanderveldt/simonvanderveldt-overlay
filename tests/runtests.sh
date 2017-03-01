2a9b90f57b8140fe6634917cddfcb03eb26e94e8
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-03-01 21:46:57 +0100

    [tests] Use repoman fork for tests
    Because there's currently no way to disable specific tests use my fork that simply disables the irrelevant tests in the code

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -6,7 +6,9 @@ export FEATURES="-news" PORTAGE_RSYNC_EXTRA_OPTS="-q"
 
 # Update the portage tree and install dependencies
 emerge --sync
-emerge -q --buildpkg --usepkg dev-vcs/git repoman
+emerge -q --buildpkg --usepkg dev-vcs/git dev-python/lxml
+# Install fork or repoman with failing tests that are irrelevant for overlays stripped out
+ebuild tests/app-portage/repoman/repoman-2.3.1.ebuild manifest clean merge
 
 # Run the tests
 repoman full
